b5d734b2e2e307c9b81f18e9fff02873250b65d4
Tankernn <frans.bergman@bomlyckan.se>
2017-08-07 17:47:02 +0200

    Add low effort web client

---
+++ client/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  var socket = new WebSocket("ws://raspberrypi:54321");
+  var keys = [];
+
+  socket.onopen = function() {
+    var sendInput = function () {
+      socket.send(JSON.stringify(keys));
+
+    };
+    setInterval(sendInput, 69);
+  }
+
+  $(document).keydown(function(e) {
+    if (keys.indexOf(e.keyCode) === -1) {
+      keys.push(e.keyCode);
+    }
+    e.preventDefault();
+  });
+
+  $(document).keyup(function(e) {
+    if (keys.indexOf(e.keyCode) !== -1) {
+      keys.splice(keys.indexOf(e.keyCode), 1);
+    }
+    e.preventDefault();
+  });
+});
